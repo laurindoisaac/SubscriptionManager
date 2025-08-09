eb7f9ae1e00d615cf35e9c3c45b3b6716e0f6c1b
Lauri <116688165+laurindoisaac@users.noreply.github.com>
2025-08-10 00:46:44 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/subscriptionmanager/main.go
@@ -0,0 +1,23 @@
+// cmd/subscriptionmanager/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "subscriptionmanager/internal/subscriptionmanager"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := subscriptionmanager.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
